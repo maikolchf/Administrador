d21707669e998cfa15aab3dae7f1d1db9524da04
maikolchf <maikolchf@outlook.com>
2022-03-02 20:29:14 -0600

    prueba

--- administradorFormularios/Proveedores/RegistrarProveedor.Designer.cs
+++ administradorFormularios/Proveedores/RegistrarProveedor.Designer.cs
@@ -34,7 +34,7 @@
             // button1
             // 
             this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button1.Location = new System.Drawing.Point(373, 215);
+            this.button1.Location = new System.Drawing.Point(303, 211);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
